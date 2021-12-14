7ffb026c59c44de77fc7c8e65bbd327e94280520
ttomz <ttomz4291@gmail.com>
2021-12-15 00:47:52 +0200

    fix filename typo

--- src/app/wish-listh/wish-list-routing.module.ts
+++ src/app/wish-listh/wish-list-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WishListhComponent } from './wish-listh.component';
+import { WishListComponent } from './wish-list.component';
 
-const routes: Routes = [{ path: '', component: WishListhComponent }];
+const routes: Routes = [{ path: '', component: WishListComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class WishListhRoutingModule { }
+export class WishListRoutingModule { }
